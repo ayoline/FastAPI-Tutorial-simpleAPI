d0db687b69ecf34ab2d821255d8e61f9fd7b4149
Anderson P Soares <ayo@outlook.com.br>
2023-02-05 19:19:29 -0300

    finish the app

--- main.py
+++ main.py
@@ -3,6 +3,29 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 
 
+app = FastAPI()
+banco = []
+
+
 class Item(BaseModel):
     id: int
     quantidade: int
+    descricao: str
+    valor: float
+
+
+@app.post("/item")
+def add_item(item: Item):
+    banco.append(item)
+    return item
+
+
+@app.get("/item")
+def list_items():
+    return banco
+
+
+@app.get("/item/total")
+def get_total():
+    total = sum([item.quantidade * item.valor for item in banco])
+    return {"valor total": total}
